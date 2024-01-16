15247e419ee34f1f9dae7eee45171d4d8f161158
Julian Alvarez <julianjear10@gmail.com>
2024-01-16 11:43:39 -0800

    Removing GA temporarily

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -2,23 +2,23 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import ReactGA from "react-ga4";
 
-const { NODE_ENV, REACT_APP_MEASUREMENT_ID } = process.env;
+// const { NODE_ENV, REACT_APP_MEASUREMENT_ID } = process.env;
 
-if (NODE_ENV === "production") {
-  ReactGA.initialize(REACT_APP_MEASUREMENT_ID);
-}
+// if (NODE_ENV === "production") {
+//   ReactGA.initialize(REACT_APP_MEASUREMENT_ID);
+// }
 
 const Analytics = () => {
-  const { pathname } = useLocation();
+  // const { pathname } = useLocation();
 
-  useEffect(() => {
-    if (NODE_ENV === "production") {
-      ReactGA.set({
-        page: pathname,
-      });
-      ReactGA.send(pathname);
-    }
-  }, [pathname]);
+  // useEffect(() => {
+  //   if (NODE_ENV === "production") {
+  //     ReactGA.set({
+  //       page: pathname,
+  //     });
+  //     ReactGA.send(pathname);
+  //   }
+  // }, [pathname]);
 
   return null;
 };
