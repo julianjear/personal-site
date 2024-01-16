b5f0871f77445984eda9b35007722959adbd0ac3
Julian Alvarez <julianjear10@gmail.com>
2024-01-16 11:45:15 -0800

    Removing GA temporarily

--- src/components/Template/Analytics.js
+++ src/components/Template/Analytics.js
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import ReactGA from "react-ga4";
+// import { useEffect } from "react";
+// import { useLocation } from "react-router-dom";
+// import ReactGA from "react-ga4";
 
 // const { NODE_ENV, REACT_APP_MEASUREMENT_ID } = process.env;
 
